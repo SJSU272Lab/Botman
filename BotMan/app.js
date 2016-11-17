d627e7c343f9f396f74c5b98a81ab065aad916cf
Sagar Mane <sagar.mane006@gmail.com>
2016-11-17 00:01:30 -0800

    Implemented GitHub login using passport

--- BotMan/app.js
+++ BotMan/app.js
@@ -13,6 +13,45 @@ var cmd 		= require('node-cmd');
 var app 		= express();
 var index		= require('./routes/index');
 
+var session = require('express-session');
+
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+var GITHUB_CLIENT_ID = "4b6982470783a2811dcd";
+var GITHUB_CLIENT_SECRET = "b3d9d2cb7fbfa0168b7a871b9238f8a10d95be0f";
+
+
+passport.serializeUser(function(user, done) {
+	  done(null, user);
+	});
+
+	passport.deserializeUser(function(obj, done) {
+	  done(null, obj);
+	});
+
+	passport.use(new GitHubStrategy({
+	    clientID: GITHUB_CLIENT_ID,
+	    clientSecret: GITHUB_CLIENT_SECRET,
+	    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    // asynchronous verification, for effect...
+	    process.nextTick(function () {
+	      
+	      // To keep the example simple, the user's GitHub profile is returned to
+	      // represent the logged-in user.  In a typical application, you would want
+	      // to associate the GitHub account with a user record in your database,
+	      // and return that user instead.
+	      return done(null, profile);
+	    });
+	  }
+	));
+	
+	
+	
+	
+	
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -21,6 +60,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,7 +73,27 @@ if ('development' === app.get('env')) {
 }
 
 app.get('/',index.loadindex);
-app.get('/home', home.loadhome);
+app.get('/home',ensureAuthenticated, home.loadhome);
+
+app.get('/auth/github',
+		  passport.authenticate('github', { scope: [ 'user:email' ] }),
+		  function(req, res){
+		    // The request will be redirected to GitHub for authentication, so this
+		    // function will not be called.
+			
+		  });
+
+
+app.get('/auth/github/callback', 
+		  passport.authenticate('github', { failureRedirect: '/' }),
+		  function(req, res) {
+		    res.redirect('/home');
+		  });
+
+app.get('/logout', function(req, res){
+	  req.logout();
+	  res.redirect('/');
+	});
 
 app.post('/nodeBot',home.nodeBot);
 http.createServer(app).listen(app.get('port'), function(){
@@ -41,4 +104,13 @@ http.createServer(app).listen(app.get('port'), function(){
 cmd.get('ls', function(data) {
 	console.log('I am at: \n', data);
 });
+
+function ensureAuthenticated(req, res, next) {
+	  if (req.isAuthenticated()) { return next(); }
+	  else{
+		  console.log("not authenticated");
+		  res.redirect('/');  
+	  }
+	  
+	}
 
